fe04bb8cb597cd39c2f89ebb8a6799e60d609db3
Bogomil Filipov (DigitalOcean vps) <filipov.bogomil@gmail.com>
2021-02-24 18:21:30 +0000

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Upgrade command failed, retrying in 5 secs...
-    sleep 5
-done
-exec gunicorn -b :5000 --access-logfile - --error-logfile - business_game:app
+#!/bin/bash
+export FLASK_APP=game.py
+export FLASK_APP_DATABASE_USER='postgres'
+export FLASK_APP_DATABASE_PASSWORD='svinqtamaika_123'
+export FLASK_APP_DATABASE_DB='business_game'
+export FLASK_APP_DATABASE_HOST='localhost'
+export FLASK_APP_DATABASE_PORT='5432'
+gunicorn -b 0.0.0.0:5001 -w 16 game:app --access-logfile /home/business_game/business_game/logs/gunicorn-access.log --error-logfile /home/business_game/business_game/logs/gunicorn-error.log
